08014837144021557a71ada28af51924782c4620
Emil Georgiev <emilgeorgievit@gmail.com>
2026-02-02 18:44:48 +0200

    Migrate Home and Loading components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import { Home } from './components/home/Home';
+import { Loading } from './components/loading/Loading';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 20000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      {isLoading && <Loading />}
 
-export default App
+      <Router>
+        <Routes>
+          <Route path="/" element={ <Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useRef, useState } from 'react';
+
+// import Resume from '../Resume/Resume';
+// import Intro from '../Intro/Intro';
+// import About from '../About/About';
+// import Portfolio from '../Portfolio/Portfolio';
+// import Contact from '../Contact/Contact';
+// import Header from '../Header/Header';
+
+export const Home: React.FC = () => {
+    const [isFixed, setIsFixed] = useState<boolean>(false);
+    const backToTopRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsFixed(window.scrollY >= 600);
+        };
+
+        document.addEventListener('scroll', handleScroll);
+
+        return () => {
+            document.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const scrollToTop = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault();
+        backToTopRef.current?.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    return (
+        <>
+            <div className="wrapper" ref={backToTopRef}>
+                {/* <Header isFixed={isFixed} />
+                <Intro isFixed={isFixed} />
+                <About />
+                <Resume />
+                <Portfolio />
+                <Contact /> */}
+
+                <a
+                    href="#home"
+                    onClick={scrollToTop}
+                    className={isFixed ? 'back-top fixed' : 'back-top'}
+                >
+                    <svg
+                        className="ico ico-back-top"
+                        width="30px"
+                        height="30px"
+                        viewBox="0 0 31.479 31.479"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M26.477,10.274c0.444,0.444,0.444,1.143,0,1.587c-0.429,0.429-1.143,0.429-1.571,0l-8.047-8.047
+              v26.555c0,0.619-0.492,1.111-1.111,1.111c-0.619,0-1.127-0.492-1.127-1.111V3.813l-8.031,8.047c-0.444,0.429-1.159,0.429-1.587,0
+              c-0.444-0.444-0.444-1.143,0-1.587l9.952-9.952c0.429-0.429,1.143-0.429,1.571,0L26.477,10.274z" />
+                    </svg>
+                </a>
+            </div>
+        </>
+    );
+};
